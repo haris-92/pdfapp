5976b8896bb09daef2a68be8fd1cc5cf75eaa96d
DESKTOP-OO24IPG\haris <hsiripothu@miraclesoft.com>
2018-09-25 21:28:01 -0400

    changes related to s3

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ var http = require('http');
 var AWS = require('aws-sdk');
 var fs = require("fs");
 var shouldFlatten = false;
-var sourcePDF = "./test.pdf";
+var sourcePDF = "./test3.pdf";
 var destinationPDF =  "./test_complete3.pdf";
 var data = {
     "last_name" : "siripothu",
@@ -31,34 +31,35 @@ var getParams = {
 router.get('/generatepdf',function(req,res,next){
 
     var s3 = new AWS.S3();
-    var Data;
-    // s3.getObject(getParams, function(err, data) {
-    //     if (err) console.log(err, err.stack); // an error occurred
-    //     else   this.Data = data.Body;        
-    // });
-
-    var url = s3.getSignedUrl('getObject', getParams);
-  console.log(url);
-    // pdfFiller.fillFormWithFlatten( url, data, shouldFlatten, function(err) {
-    //     if (err) console.log(err);
-    //     console.log("In callback (we're done).");
-    // });
-    pdfFiller.fillFormWithFlatten(sourcePDF, data, shouldFlatten)
-    .then((outputStream) => { const Body = outputStream;
-    const Bucket = 'pdfbucket12';
-    const Key = 'test_complete3.pdf';
-    const ContentType = 'application/pdf';
+    const file = require('fs').createWriteStream('./test3.pdf');
     
-    const uploader = new AWS.S3.ManagedUpload({
-        params: {Bucket, Key, Body, ContentType},
-        service: s3,
+    const s3Promise = s3.getObject(getParams).promise();
+    
+    s3Promise.then((data) => {
+      file.write(data.Body, () => {
+        file.end();
+        
+      });
+    }).then(()=>{
+        pdfFiller.fillFormWithFlatten(sourcePDF, data, shouldFlatten)
+        .then((outputStream) => { const Body = outputStream;
+        const Bucket = 'pdfbucket12';
+        const Key = 'test_complete10.pdf';
+        const ContentType = 'application/pdf';
+        
+        const uploader = new AWS.S3.ManagedUpload({
+            params: {Bucket, Key, Body, ContentType},
+            service: s3,
+        });
+        
+        uploader.promise().then((data) => {/* do something with AWS response */})
+        
+    }).catch((err) => {
+        console.log(err);
     });
-    
-    uploader.promise().then((data) => {/* do something with AWS response */})
-    
-}).catch((err) => {
-    console.log(err);
-});
+    });
+  
+ 
 
     res.send('File generated in s3');
     
